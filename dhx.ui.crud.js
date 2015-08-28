bc66171d4273b41671f9fecf7f4dca57da7c5040
web2solutions <eduardo@web2solutions.com.br>
2015-08-28 14:23:15 -0300

    assets

--- dhx.ui.crud.js
+++
@@ -1,8 +0,0 @@
-/*jslint browser: true, devel: true, eqeq: true, newcap: true, nomen: true, white: true */
-/*global $dhx, dhtmlx, dhtmlXLayoutObject, dbDemo */
-$dhx.ui.crud = {
-    version: '1.0.3',
-    controller: {},
-    view: {},
-    settings: {}
-};
